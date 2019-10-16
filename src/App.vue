aab8aef0cd775c082a4df94e1380c8fa103cc1b4
tricky <517672932@qq.com>
2019-10-16 15:53:51 +0800

    完成计算器的功能

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
   <div id="app">
       <div class="calculator-body">
-          <div class="led">123123</div>
+          <div class="led">{{led}}</div>
           <div class="btn-container">
             <div class="btn-top">
-              <my-btn orange>AC</my-btn>
-              <my-btn blue>/</my-btn>
-              <my-btn blue>*</my-btn>
-              <my-btn blue>-</my-btn>
+              <my-btn orange @click="allclear">AC</my-btn>
+              <my-btn blue @click="pressOperator('/')">/</my-btn>
+              <my-btn blue @click="pressOperator('*')">*</my-btn>
+              <my-btn blue @click="pressOperator('-')">-</my-btn>
             </div>
             <div class="btn-bottom">
               <div class="btn-left">
-                <my-btn>7</my-btn>
-                <my-btn>8</my-btn>
-                <my-btn>9</my-btn>
-                <my-btn>4</my-btn>
-                <my-btn>5</my-btn>
-                <my-btn>6</my-btn>
-                <my-btn>1</my-btn>
-                <my-btn>2</my-btn>
-                <my-btn>3</my-btn>
-                <my-btn wide>0</my-btn>
-                <my-btn>.</my-btn>
+                <my-btn @click="pressNumber(7)">7</my-btn>
+                <my-btn @click="pressNumber(8)">8</my-btn>
+                <my-btn @click="pressNumber(9)">9</my-btn>
+                <my-btn @click="pressNumber(4)">4</my-btn>
+                <my-btn @click="pressNumber(5)">5</my-btn>
+                <my-btn @click="pressNumber(6)">6</my-btn>
+                <my-btn @click="pressNumber(1)">1</my-btn>
+                <my-btn @click="pressNumber(2)">2</my-btn>
+                <my-btn @click="pressNumber(3)">3</my-btn>
+                <my-btn wide @click="pressNumber(0)">0</my-btn>
+                <my-btn @click="pressNumber('.')">.</my-btn>
               </div>
               <div class="btn-right">
-                <my-btn blue long>+</my-btn>
-                <my-btn orange long>=</my-btn>             
+                <my-btn blue long @click="pressOperator('+')">+</my-btn>
+                <my-btn orange long @click="equalHandler">=</my-btn>             
               </div>
             </div>
           </div>
@@ -39,6 +39,75 @@ export default {
   name: 'app',
   components: {
     MyBtn
+  },
+  data(){
+    return {
+      led:"",
+      numOne:0,
+      numTwo:0,
+      result:0,
+      operator:"",
+      isInputingNumTwo:false,
+      isNewRound:false,
+      // 是否拥有小数点
+      hasDecimalPoint:false,
+    }
+  },
+  methods:{
+    pressNumber(num){
+      // 处理小数点
+      if(num === "."){
+        if(this.hasDecimalPoint){
+          return;
+        }
+        this.led = this.led+ ".";
+        this.hasDecimalPoint = true;
+        return;
+      }
+      // 处理数字
+      if(this.operator==="" || this.isInputingNumTwo){
+          if(this.isNewRound){
+            this.led = num+"";
+            this.isNewRound = false;
+            return;
+          }
+          this.led = this.led+num;
+          return;
+      }
+      this.led = num+"";
+      this.isInputingNumTwo = true;
+    },
+    allclear(){
+      this.led = "";
+    },
+    pressOperator(ope){
+      this.numOne = parseFloat(this.led);
+      this.operator = ope;
+      this.hasDecimalPoint = false;
+    },
+    equalHandler(){
+      this.numTwo = parseFloat(this.led);
+      switch(this.operator){
+        case "+":
+          this.led = parseFloat((this.numOne+this.numTwo).toPrecision(12))+"";
+          break;
+        case "-":
+          this.led = parseFloat((this.numOne-this.numTwo).toPrecision(12))+"";
+          break;
+        case "*":
+          this.led = parseFloat((this.numOne*this.numTwo).toPrecision(12))+"";
+          break;
+        case "/":
+          this.led = parseFloat((this.numOne/this.numTwo).toPrecision(12))+"";
+          break;
+      }
+      this.operator = "";
+      this.numOne = 0;
+      this.numTwo = 0;
+      this.isNewRound = true;
+      this.isInputingNumTwo = false;
+      this.hasDecimalPoint = false;
+    }
   }
 }
 </script>
